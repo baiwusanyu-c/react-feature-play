b0d0257daebb374c789040e80e36b4d68f05de64
baiwusanyu <740132583@qq.com>
2022-09-13 00:46:26 +0800

    chore: split chunk

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mkcert from "vite-plugin-mkcert";
 import WindiCSS from 'vite-plugin-windicss'
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
       react(),
-      WindiCSS()
-  ]
+      WindiCSS(),
+       chunkSplitPlugin({
+           // 指定拆包策略
+           customSplitting: {
+               'react-vendor': ['react', 'react-dom','react-router-dom','antd'],
+               'utils-vendor': [
+                   /src\/utils/
+               ],
+               //'lodash': ['lodash'],
+               // /src\/router/,
+               // /src\/components/
+               //'library': ['antd'],
+           }
+       }),
+      mkcert(),
+  ],
+    server: {
+        host:'0.0.0.0',
+        // https 选项需要开启
+        https: true,
+        port:3000,
+    },
+    build:{
+      minify:false,
+      cssCodeSplit:true
+    }
 })
+
+
